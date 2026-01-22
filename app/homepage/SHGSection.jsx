3a4e6e313f2ba88ef43a85ea3959c72da8ce0481
satyam <sqautilya@gmail.com>
2026-01-22 19:21:49 +0530

    this

--- app/homepage/SHGSection.jsx
+++ app/homepage/SHGSection.jsx
@@ -1,8 +1,15 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const SHGSection = () => {
+	const router = useRouter();
 	return (
 		<div
+			onClick={() => {
+				router.push('/my-shg');
+				// Add your click handler logic here
+			}}
 			style={{
 				backgroundImage: `url('https://8dxblayock8syelc.public.blob.vercel-storage.com/homepage/shgbg.png')`,
 				backgroundSize: 'cover',
