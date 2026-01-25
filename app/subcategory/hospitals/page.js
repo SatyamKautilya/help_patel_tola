0fe76e2912450aead9e5edbc9111599272927535
satyam <sqautilya@gmail.com>
2026-01-25 12:38:35 +0530

    ui

--- app/subcategory/hospitals/page.js
+++ app/subcategory/hospitals/page.js
@@ -165,7 +165,7 @@ export default function App() {
 			</AnimatePresence>
 
 			{/* Main Content */}
-			<main className='pt-[120px] pb-10 px-4 max-w-7xl mx-auto relative z-10'>
+			<main className='pt-[90px] pb-10 px-4 max-w-7xl mx-auto relative z-10'>
 				<AnimatePresence mode='wait'>
 					{!loading && (
 						<motion.section
