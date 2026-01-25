4ecc9a3da50b110dfeefab8455d8943706fafba3
satyam <sqautilya@gmail.com>
2026-01-25 13:07:49 +0530

    this

--- app/admin/page.js
+++ app/admin/page.js
@@ -25,8 +25,8 @@ const hasAccess = (userGroups, action) => {
 
 const AdminDashboard = () => {
 	const thisUser = useSelector((state) => state.appContext.user);
-	// const userGroups = thisUser?.userGroups || [];
-	const userGroups = ['shg_onboarder'];
+	const userGroups = thisUser?.userGroups || [];
+	// const userGroups = ['shg_onboarder'];
 
 	/** ✅ tabs are DERIVED, not state */
 	const tabs = useMemo(() => {
